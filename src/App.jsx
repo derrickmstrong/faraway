6fd927a9610d1116beb8c63d3cf2da3dba7701f8
Derrick Strong <derrickmstrong@gmail.com>
2024-10-24 16:27:49 -0500

    Add Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import Logo from "./components/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
   return (
     <>
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      {isAuthenticated ? (
+        <>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Logout
+          </button>
+          <Form />
+          <PackingList />
+          <Stats />
+        </>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Login</button>
+      )}
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
-import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory";
 import Spinner from "../src/components/Spinner/Spinner.jsx";
 import "./index.css";
 
@@ -12,9 +14,13 @@ const renderApp = () => {
   document.body.classList.remove("loading");
   root.render(
     <StrictMode>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <Router>
+        <Auth0ProviderWithHistory>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </Auth0ProviderWithHistory>
+      </Router>
     </StrictMode>
   );
 };
