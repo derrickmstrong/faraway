485701b598554869e9a098774b24275e8f2bcf92
Derrick Strong <derrickmstrong@gmail.com>
2024-10-24 17:06:40 -0500

    Refactor App component to conditionally render components based on authentication status

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import Logo from "./components/Logo/Logo";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <>
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      {isAuthenticated ? (
+        <>
+          <Form />
+          <PackingList />
+          <Stats />
+        </>
+      ) : null}
     </>
   );
 };
