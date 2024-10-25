7aeb6214593fdc2f1d5dd1b5b0510b63afe9a3a1
Derrick Strong <derrickmstrong@gmail.com>
2024-10-25 14:46:44 -0500

    Update Auth0Provider to user localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,11 @@ import Stats from "./components/Stats";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
@@ -19,7 +23,7 @@ const App = () => {
         </>
       ) : (
         <div>
-          <h1 style={{ background: "#e5771f" }}>FarAway</h1>
+          <h1 className="title">FarAway</h1>
           <h2>The Trip Planning App</h2>
           <p className="welcome-message">
             Please log in to start adding items to your packing list.
