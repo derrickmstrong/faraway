ab6e92b1b87ca13f94ba86945bf3c8de2ad5ccf5
Derrick Strong <derrickmstrong@gmail.com>
2024-10-25 14:57:18 -0500

    Cont. Testing

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,30 +1,28 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import "./Logo.css";
+import "./Logo.css"; // Assuming you have a CSS file for styling
 
 const Logo = () => {
   const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
-  console.log('isAuthenticated', isAuthenticated);
-  console.log('user', user);
+  console.log("isAuthenticated", isAuthenticated);
+  console.log("user", user);
 
   return (
     <div className="logo-container">
-      <span className="">🌴 FarAway 🧳</span>
-      {isAuthenticated ? (
-        <button
-          className="auth-button"
-          onClick={() => logout({ returnTo: window.location.origin })}
-        >
-          Logout
-        </button>
-      ) : (
-        <div className="auth-button">
-          {isAuthenticated && user && (
-            <span className="user-name">Welcome, {user.name}</span>
-          )}
-          <button className="" onClick={() => loginWithRedirect()}>
-            Login
+      <span className="logo-text">🌴 FarAway 🧳</span>
+      {isAuthenticated && user ? (
+        <div className="user-info">
+          <span className="user-name">Welcome, {user.name}</span>
+          <button
+            className="auth-button"
+            onClick={() => logout({ returnTo: window.location.origin })}
+          >
+            Logout
           </button>
         </div>
+      ) : (
+        <button className="auth-button" onClick={() => loginWithRedirect()}>
+          Login
+        </button>
       )}
     </div>
   );
