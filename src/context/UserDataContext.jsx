099c23a63bb98da03c31dfce9e86b934cd2efcfa
Derrick Strong <derrickmstrong@gmail.com>
2024-10-25 21:38:53 -0500

    Revert "Refactor UserDataProvider to store user data in user-specific local storage"
    This reverts commit e4f8ae2105b46f8a90e02c5f901409e1ee3b9873.

--- src/context/UserDataContext.jsx
+++ src/context/UserDataContext.jsx
@@ -1,78 +1,32 @@
 import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { useAuth0 } from "@auth0/auth0-react";
 
+// Create a Context for the theme
 const UserDataContext = createContext();
 
 export const UserDataProvider = ({ children }) => {
-  const { isAuthenticated, user } = useAuth0();
-
-  const initialUserData = () => {
-    console.log('AM I AUTHENTICATED?', isAuthenticated);
+  const [userData, setUserData] = useState(() => {
     // Load userData from local storage if available
-    if (isAuthenticated) {
-      const userKey = `userData-${user.nickname}`;
-      const storedUserData = localStorage.getItem(userKey);
-      console.log("storedUserData", storedUserData);
-      return [...storedUserData];
-    }
-  };
-  const [userData, setUserData] = useState(initialUserData());
-
-  // useEffect(() => {
-  //   if (isAuthenticated && user) {
-  //     try {
-  //       const userKey = `userData-${user.sub}`;
-  //       const storedUserData = localStorage.getItem(userKey);
-  //       console.log("storedUserData", JSON.parse(storedUserData));
-  //       if (storedUserData) {
-  //         setUserData(JSON.parse(storedUserData));
-  //       } else {
-  //         const initialUserData = {
-  //           id: user.sub,
-  //           description: "",
-  //           quantity: 1,
-  //           packed: false,
-  //         };
-  //         localStorage.setItem(userKey, JSON.stringify(initialUserData));
-  //         setUserData(initialUserData);
-  //       }
-  //     } catch (error) {
-  //       console.error(
-  //         "Error accessing or parsing user data from localStorage:",
-  //         error
-  //       );
-  //     }
-  //   }
-  // }, [isAuthenticated, user]);
+    const savedItems = localStorage.getItem("userData");
+    return savedItems ? JSON.parse(savedItems) : [];
+  });
 
   useEffect(() => {
     // Save userData to local storage whenever they change
-    if (isAuthenticated && user) {
-      const userKey = `userData-${user.nickname}`;
-      localStorage.setItem(userKey, JSON.stringify(userData));
-    }
-  }, [isAuthenticated, user, userData]);
+    localStorage.setItem("userData", JSON.stringify(userData));
+  }, [userData]);
 
   const handleAddItems = (newItem) => {
-    if (isAuthenticated && user) {
-      try {
-        const userKey = `userData-${user.nickname}`;
-        localStorage.setItem(userKey, JSON.stringify(newItem));
-        setUserData((prevItems) => [newItem, ...prevItems]);
-      } catch (error) {
-        console.error("Error updating user data in localStorage:", error);
-      }
-    }
+    setUserData((prevItems) => [newItem, ...prevItems]);
   };
 
   const handleDeleteItem = (id) => {
-    setUserData((prevItems) => prevItems?.filter((item) => item.id !== id));
+    setUserData((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
   const handleToggleItem = (id) => {
     setUserData((prevItems) =>
-      prevItems?.map((item) =>
+      prevItems.map((item) =>
         item.id === id ? { ...item, packed: !item.packed } : item
       )
     );
